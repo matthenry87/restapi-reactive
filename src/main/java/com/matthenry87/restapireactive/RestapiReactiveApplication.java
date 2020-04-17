ac072999dfa987582481889fce7b0119fa864143
Matt Henry <matthenry87@gmail.com>
2020-04-16 18:27:32 -0600

    Project generated from start.spring.io

---
+++ src/main/java/com/matthenry87/restapireactive/RestapiReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.matthenry87.restapireactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matthenry87/restapireactive/RestapiReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthenry87.restapireactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
